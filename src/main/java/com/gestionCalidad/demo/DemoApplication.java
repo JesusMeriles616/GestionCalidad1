d744e8e3be483a6578b8459fe53789a6b5ad7659
JesusMeriles <jesus.meriles@ucb.edu.bo>
2025-05-07 02:42:45 -0400

    cambio a jar

--- src/main/java/com/gestionCalidad/demo/DemoApplication.java
+++ src/main/java/com/gestionCalidad/demo/DemoApplication.java
@@ -2,19 +2,11 @@ package com.gestionCalidad.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication {
 
-	 @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DemoApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
 }

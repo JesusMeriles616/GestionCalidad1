f4d5040242754a074b5d57b306edfd3314e61f93
O1zoner <149082999+O1zoner@users.noreply.github.com>
2025-05-18 12:26:17 -0400

    Typo en TresEnRayaTest.java

--- src/test/java/com/gestionCalidad/TresEnRayaTest.java
+++ src/test/java/com/gestionCalidad/TresEnRayaTest.java
@@ -99,7 +99,7 @@ public class TresEnRayaTest {
         char esperado = 'X';
         char jugadorActual = juego.getJugadorActual();
         
-        assumeTrue("No es el primer turno, se salta esta prueba", juego.getTurno == 1);
+        assumeTrue("No es el primer turno, se salta esta prueba", juego.getTurno() == 1);
         assertEquals(esperado, jugadorActual);
     }
 
